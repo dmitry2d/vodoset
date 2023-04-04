a0cf9059b4560f7d992be9965dc52e28ae4e6fa2
dmitry2d <volcharo@gmail.com>
2023-04-04 16:43:53 +0300

    code

--- dev/frontend/src/store/session/auth.js
+++ dev/frontend/src/store/session/auth.js
@@ -1,25 +1,38 @@
 import { reactive } from 'vue';
 
-export default function (state) {
+export default function (store) {
 
-    state.auth = reactive({
+    const state = reactive({
         token: null,
         username: null,
         role: null
-    })
+    });
 
+    
     const check = function () {
+        /*
+  
+        get local token
+        if not return
+        if yes call api auth
+        if return token - set new token and user
+        if not clear state
 
+*/
+        const token = localStorage.getItem ('authorization-token');
+        
+        
+        
     }
-
+    
     const getLocalToken = function () {
-
+        
     }
+    
+    store.state.auth = state;
 
     return {
-
         check
-
     };
 
 }
